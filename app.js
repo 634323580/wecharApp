52bd49dce34d71347a6ed8585d43a6fc53c44e81
ChenZhiPeng <634323580@qq.com>
2017-01-06 17:29:50 +0800

    电影页面

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 App({
     gloabalData: {
         g_isPlatIngMusic: false,
-        g_currentMusicPostId: null
+        g_currentMusicPostId: null,
+        doubanBase: 'https://api.douban.com'
     }
 })
--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,19 +1,101 @@
 // pages/movies/movies.js
+let app = getApp();
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  data: {
+    inTheaters: {},
+    comingSoon: {},
+    top250: {},
   },
-  onReady:function(){
-    // 页面渲染完成
+  onLoad: function (options) {
+    let inTheatersUrl = `${app.gloabalData.doubanBase}/v2/movie/in_theaters?start=0&count=3`;
+    let comingSoonUrl = `${app.gloabalData.doubanBase}/v2/movie/coming_soon?start=0&count=3`;
+    let top250Url = `${app.gloabalData.doubanBase}/v2/movie/top250?start=0&count=3`;
+
+    //  这样请求纯粹为了学习promise
+    this.getmovieListData(inTheatersUrl, 'inTheaters')
+        .then(res => {
+          return this.getmovieListData(comingSoonUrl, 'comingSoon');
+        })
+        .then(res => {
+          return this.getmovieListData(top250Url, 'top250');
+        })
+        .then(res => {
+        })
   },
-  onShow:function(){
-    // 页面显示
+  getmovieListData: function (url, settedKey) {
+    return new Promise((resolve, reject) => {
+      wx.request({
+        url: url,
+        methods: 'GET',
+        header: {
+          'Content-Type': 'json'
+          // 'Content-Type': 'application/xml'
+        },
+        success: (res) => {
+          resolve(res);
+          this.processDoubanData(res.data, settedKey);
+        },
+        fail: function(err) {
+          reject(err);
+        }
+      })
+    })
   },
-  onHide:function(){
-    // 页面隐藏
+  computedStart: function(number) {
+      let starState = [];
+
+      number = number.split('').join('.')
+
+      // let score = Math.floor(number * 2) / 2;
+
+     let integer = Math.floor(number);
+      for(var i = 0; i < integer; i++) {
+        starState.push('star');
+      }
+
+      let hasDecimal = number % 1 !== 0;
+      if(hasDecimal) {
+        starState.push('none-star');
+      }
+
+      while(starState.length < 5) {
+        starState.push('chat');
+      }
+      console.log(starState)
+      return starState;
   },
-  onUnload:function(){
-    // 页面关闭
+
+  processDoubanData: function(moviesDouban, settedKey) {
+    console.log(moviesDouban)
+    var movies = [];
+     for(var idx in moviesDouban.subjects) {
+       let subject = moviesDouban.subjects[idx];
+       let title = subject.title;
+       if(title.length >= 6) {
+         title = title.substring(0, 6) + '...';
+       }
+       let startNumber = subject.rating.stars;
+       let starState = this.computedStart(startNumber);
+       let temp = {
+         title: title,
+         average: {
+           starState: starState,
+           number:subject.rating.average
+         },
+         coverageUrl: subject.images.large,
+         movieId: subject.id
+       }
+
+       movies.push(temp);
+       var readyData = {};
+       readyData[settedKey] = {
+         movieListTitle: moviesDouban.title,
+         movies: movies
+       };
+       this.setData(readyData);
+      //  this.setData({
+      //    movies: movies
+      //  });
+     }
   }
 })
